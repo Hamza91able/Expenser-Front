0cb6b06d05490f002ff63251f1a273874625b955
Hamza91able <m.hamzakhan91able@gmail.com>
2021-08-17 00:09:03 +0500

    Changed connection string

--- src/Utils/connectionString.js
+++ src/Utils/connectionString.js
@@ -1,1 +1,10 @@
-export const connection_string = "http://localhost:8000/api/v1";
+export let connection_string;
+export let image_url;
+
+if (window.location.hostname == "localhost") {
+  connection_string = "http://localhost:8000/api/v1";
+  image_url = "http://localhost:8000/";
+} else {
+  connection_string = "https://expenser-back.herokuapp.com/api/v1";
+  image_url = "https://expenser-back.herokuapp.com/";
+}
